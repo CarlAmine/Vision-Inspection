ccab6aa99b9ea891fca6d610830e53e462e86ac9
Carl Amine <carlamine04@hotmail.com>
2026-05-02 22:57:50 +0300

    Fix ImportError: export PipelineConfig in pipeline/__init__.py

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -3,12 +3,13 @@ VisualInspector Pipeline
 ~~~~~~~~~~~~~~~~~~~~~~~~
 End-to-end machine vision inspection pipeline.
 """
-from .inspector import InspectionPipeline
+from .inspector import InspectionPipeline, PipelineConfig
 from .preprocessor import ImagePreprocessor
 from .result import InspectionResult, Defect, DefectSeverity
 
 __all__ = [
     "InspectionPipeline",
+    "PipelineConfig",
     "ImagePreprocessor",
     "InspectionResult",
     "Defect",
